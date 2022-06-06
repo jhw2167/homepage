5c0721f79c5e05d7227730101397f112360c5bac
jhw2167 <jhw2167@columbia.edu>
2022-06-05 21:36:07 -0500

    Real time window data wworks

--- src/main/pages/Homepage.tsx
+++ src/main/pages/Homepage.tsx
@@ -1,24 +1,26 @@
 //React Imports
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 //Project Imports
 
 //misc
 import * as c from '../../resources/constants';
-import useWindowDimensions from '../subcomponents/misc/WindowDims';
+import { useWindowSize } from '../subcomponents/misc/WindowDims';
 
 
 
 function Homepage() 
 {
-    const [windowDims] = useState<c.Dims2D>(useWindowDimensions());
+    //const [windowDims, setWindowDims] = useState<c.Dims2D>(useWindowDimensions());
+    let [h, w] = useWindowSize();
 
+    
     return (
       <div className="container-fluid">
           <div className="row outer-row">
             <p> This is a normal homepage </p>
-            <p> {windowDims.h} , {windowDims.w}  </p>
+            <div> {h} , {w}  </div>
           </div> {/*END OUTER ROW */}
       </div> 
     ); {/*END WRAPPER CONTAINER */}
--- src/main/subcomponents/misc/WindowDims.tsx
+++ src/main/subcomponents/misc/WindowDims.tsx
@@ -1,20 +1,21 @@
-import { useState, useEffect } from 'react';
 
-export default function useWindowDimensions() {
-  let dims = {
-    w : window.innerWidth,
-    h : window.innerHeight
-  }
-
-    useEffect(() => {
-      function handleResize() {
-      dims.w = window.innerWidth;
-      dims.h = window.innerHeight;
-      }
-      window.addEventListener('resize', handleResize)
-      return () => { window.removeEventListener('resize', handleResize)   }
-  });
+/*
+  Custom hook for getting real time window sizes
+*/
 
 
-  return dims;
+import { useState, useEffect, useLayoutEffect } from 'react';
+
+
+export function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
 }
